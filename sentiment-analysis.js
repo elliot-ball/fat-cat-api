fe9c88ee8b0bf438e21f5edf15d822cee998ff47
Elliot <elliotrball@gmail.com>
2021-01-16 17:17:24 +0000

    switched to a count keyword approach, sentiment to complex for now

--- sentiment-analysis.js
+++ sentiment-analysis.js
@@ -2,58 +2,65 @@ const aposToLexForm = require('apos-to-lex-form');
 const natural = require('natural');
 const SpellCorrector = require('spelling-corrector');
 const SW = require('stopword');
+var Sentiment = require('sentiment');
 
-// init
-const spellCorrector = new SpellCorrector();
-
-spellCorrector.loadDictionary('./dictionary.txt');
-
+// for each game
+//     for each review
+//         for each sentence
+//             identify keyword matches
+//             identify which keyword has the most matches
+//             sentiment analysis on sentence
+            
 
 // find keywords in reviews
-function isMatchedWord (tokenizedReview, stringReview) {
-    let match = false;
-    let matches = [];
-    const keyWords = [
-        { name: 'grindy', words: ['grindy', 'grind'], phrases: [] },
-        { name: 'adverts', words: ['advert', 'adverts', 'ad', 'ads', 'advertisement', 'advertisements'], phrases: [] },
-        { name: 'lootboxes', words: ['lootbox', 'lootboxes', 'crate'], phrases: [] },
-        { name: 'timers', words: ['wait', 'timers'], phrases: [] },
-        { name: 'pay to win', words: ['p2w'], phrases: ['pay to win']},
-        { name: 'free to play', words: ['f2p',], phrases: ['free to play'] },
+function isMatchedWord (givenWordArray) {
 
+    const keywords = [
+        { name: 'grindy', keywords: ['grindy', 'grind'], phrases: [] },
+        { name: 'adverts', keywords: ['advert', 'adverts', 'ad', 'ads', 'advertisement', 'advertisements'], phrases: ['too many ads', 'too much ads'] },
+        { name: 'lootboxes', keywords: ['lootbox', 'lootboxes', 'crate'], phrases: [] },
+        { name: 'timers', keywords: ['wait', 'timers'], phrases: [] },
+        { name: 'pay to win', keywords: ['p2w'], phrases: ['pay to win']},
+        { name: 'free to play', keywords: ['f2p',], phrases: ['free to play'] },
     ];
 
-    keyWords.forEach((keyWord) => {
-        keyWord.words.forEach((word) => {
-            tokenizedReview.forEach((token) => {
-                // console.log(`${tokenizedReview} === ${word}`);
-                if (token === word) {
-                    match = true;
-                    matches.push(keyWord.name);
+    let keywordCount = {
+        grindy: 0,
+        adverts: 0,
+        lootboxes: 0,
+        timers: 0,
+        payToWin: 0,
+        freeToPlay: 0,
+    }
+
+    keywords.forEach((keyWord) => {
+        keyWord.keywords.forEach((word) => {
+            givenWordArray.forEach((givenWord) => {
+                if (givenWord === word) {
+                    keywordCount[keyWord.name]++;
                 }
             });
         });
-        keyWord.phrases.forEach((phrase) => {
-            if (stringReview.indexOf(phrase) > 0) {
-                match = true;
-                matches.push(keyWord.name);
-            }
-        });
+        // keyWord.phrases.forEach((phrase) => {
+        //     if (stringReview.indexOf(phrase) > 0) {
+        //         keyWordCount[keyWord.name]++;
+        //     }
+        // });
     });
-    return matches;
+
+    return keywordCount;
 };
 
-// define text
-// const text = "I'm the king 420. The single gratest King in all of the land.";
-const reviews = [
+// define textx
+const reviews1 = [
     "Worst game I've ever played. Grindy and greedy. Everthing need gem, if not, you lose everytime or wait for years so you can get the card you want. Even so, the chance of win is small. Event sucks ass, too many strong AI opponent too with little to no worthy rewards. Event exclusive cards and character, because if you don't play this game long enough then f u. Whales everywhere. Cancerous community, especially Indonesian. Go play offline games, or if you love ygo just play offline ygo tcg",
     "Very bad game, lost all my data spent hours over hours playing and they won't even help me get my data back I wouldn't recommend this game at all, its also a pay to win game.",
     "Love how you can have fun even as a f2p",
     "Too pay to win",
     "Pay to win , you need 50k gems for a complete competitive deck (2000$).",
-];
+// ];
 
-const unhelpfulReviews = [
+// const unhelpfulReviews = [
     "Awesome game! Slow start but give it a chance.",
     "Excellent endless cards too choose between and combination too try",
     "The best game ever 💯",
@@ -200,46 +207,257 @@ const unhelpfulReviews = [
     "Doesnt work",
     "I think is super duper AWESOME",
 ];
-reviews.forEach((text) => {
 
-    // expand contractions - e.g. I'm -> I am
+const reviews2 = [
+    "All right",
+    "ads",
+    "everytime the game starts to play it will always crash on my note 20 ultra",
+    "the idea ia good but the game needs to be way cooler ,",
+    "Too much ads and very laggy",
+    "Nuuub game. With too many ads",
+    "Crashing everywhere",
+    "Nice",
+    "Yeah... not gonna watch an ad after every race. Uninstalled.",
+    "Theres an ad after every 10 second race.",
+    "super outstanding",
+    "After every game it shuts down",
+    "Bekar hai bhai mat download karna",
+    "Terrible game. App crashes constantly",
+    "it crashes",
+    "an awesome game, but it needs some car modifications",
+    "Mildy entertaining. Ad every 15 seconds.",
+    "good",
+    "ads are already annoying after one race",
+    "nife game",
+    "I love itttt",
+    "Super game",
+    "Imma be honest this game wen i try to get the 3x thing at th end it does not let me and guess what A ADD POPS UP WEN I START A NEW LEVEL MAKE IT WORK IM UNSTAILING IT Yall need to rate 1 star 2",
+    "This game sooooo bad 30s or 40s ad after every game is not like the ad I watched and it will waste your time for nothing",
+    "ok",
+    "Deleted before the 10 second training session ended 😅 so terrible and boring",
+    "Not sure why I turn off the vibration and it continues to vibrate and I hate that so much!🤬 I loathe vibration in a game its just so damn annoying and the fact that in the settings you can \"turn it off\" even though it actually doesn't do anything is why I'm deleting this game and giving it such a terrible review!",
+    "wow",
+    "good",
+    "too easy",
+    "Yeah , like the other reviewers says. its all about Ads. i uninstall it quick, i just try the game.",
+    "This game is for children and they asking to be over 16 to be able to play this game what the **** is this game.",
+    "Not a good game",
+    "Boring. Needs a lot of improvements.",
+    "It sucks to many adds race after race and makes your phone glitch out",
+    "boring game ever",
+    "Ads suddenly pop out during game and it occur on every stage! N there is another one after each game! Seriously?",
+    "Not like the advertisements. Too many ads within the game as well. But the game is a neat concept.",
+    "Women spamming continuously. I wish i could upload the advertisement video which you guys put up in a game ad. Seriously if that is a way of luring users for your app you suck at it. I wish i could also upload screenshot of so called chats the girls or women in your app do. It sucks totally.",
+    "Ads.",
+    "sujith",
+    "Nice game",
+    "i downloaded this app just to rate one star for braindead advertisement gameplay",
+    "awesome",
+    "game per",
+    "something is lacking",
+    "Ads here and there. Need to watch ads after clearing any single stage. Not going to play.",
+    "nice",
+    "good",
+    "to many ads",
+    "Too many ads",
+    "This is good one gaming app and top speed car racing",
+    "MAS RAFFI",
+    "they lied to us",
+    "boring",
+    "Boring, just the fact alone that turning off vibrating in the game doesnt actually do anything is enough for an uninstall",
+    "gaer new",
+    "cool",
+    "Full of adds🖕🖕🖕🖕",
+    "nice game and graphics too!",
+    "Pathetic Game",
+    "To many ads not enough game.",
+    "Vjg",
+    "nice",
+    "Just keeps crashing",
+    "2 add pop up minimum in 1 minute, this is a very horible game experience.",
+    "I did 24 levels that took about ten seconds each, I had to watch a 30 second ad after each level.",
+    "Im going to be honest its fun but tooooo much ads",
+    "To easy after every \"Race\" theirs a ad",
+    "Won all the cars except unique have up at 65 after not winning any new cars repeatedly. Took an hour to get bored of the game lol",
+    "it's a cool game",
+    "Worst game grafics is not at all good",
+    "Somewhat broken. Needs a lot of work on game mechanics, and PLEASE less ads. The concept is fun, but very hard to progress without watching ads. If you play this, turn off wifi and data.",
+    "Its ok but the option to turn off vibrations doesn't work, I even tried opening and closing app and clicking option to turn vibro off again and it does not work, I can't deal with the games constant vibration",
+    "nice",
+    "so, many, ads.",
+    "💕",
+    "Crashed more than a derby car... Literally crashed 8 times in about 3 minutes. Only made it through one race. Downloaded it, and immediately needed a 44mb update...",
+    "hell",
+    "Ok, its an AMAZING idea but seriosly what car person would even give this three stars because literally all you do is tap you cant even swipe from gear to gear and you cant really slow down for the ramps so you dont hit the nose of the car or anything and you dont get that sweet feel when you get a perfect shift or am i the only person who gets that? But still its a very half assed crapy game",
+    "hredysk",
+    "Ok",
+    "This sucks. Any car you get has the same speed limit. To dang easy. So many ads.",
+    "nice move",
+    "Fun for a little but way too many ads, physics don't even try to make sense (ie car standing on its nose for 5 hours), vibration doesn't actually turn off",
+    "this game is terrible I playee 2 levels and now my life expectancy went down 20 years do not recommend",
+    "In the settings you provide an option to turn off the vibrations but it doesn't turn them off",
+    "Cool",
+    "Very nice game",
+    "To many ads there shouldn't be an ad after every single race and no option to pay to not have any ads",
+    "ads",
+    "The boys are too easy their soo trash🗑️",
+    "Good concept, bad game Every time I start a race the game crashes, ads aren't a problem, there's a glitch where you can exit out of the app and restart in less than a few seconds while skipping the ad, very poorly made though",
+    "More ads than actual gameplay (if you can call it that). You actually just press a few buttons and wait for it to be over.",
+    "To many adds",
+    "Typical, way to many ads and overall repetitive",
+    "Keeps crashing on a new pixel 4a. Can't play past 2nd level, can't put in name. Uninstalling",
+    "Game crashes seconds after loading. It is unplayable",
+    "fun so far",
+    "Some advertismemts don't work to claim a car... I click the video but doesn't pull up last one was for a dragster lvl 15 fix it???",
+    "game just crashes non stop",
+    "Downloaded it and then automatically had to update what the heck",
+    "To many ads",
+    "Game keeps crashing",
+    "So Awesome",
+    "Epic butt cheeks",
+    "too many ads and sells your data",
+    "glitchy",
+    "Awful game. Way too easy, crashes constantly, nothing but ads. Stupid waste of time cash grab",
+    "Car constantly flips if you get too much air even if you try and down shift to cut things back. I enjoy the concept but executed poorly",
+    "Lame",
+    "it crashes every second",
+    "I say no thanks and get ads anyway? But no extra reward lmao this game is trash",
+    "Good idea, horrible game. Game crashes after every race for me.",
+    "Force close, can not even play the game. Note20Ultra",
+    "Keeps crashing right off the start,",
+    "Fun idea, but crashes after EVERY race. Idk if just me, but it's a real shame.",
+    "Boring",
+    "Game is a serious joke.... when you finish, there is a multiplier at the end. Each time I have it the full way up and I only get it to 9×... sorry but this is very sad...",
+    "Horrible, I have a note 20 ultra and the game won't even play. If you are going to make a game, at least make it work for newer phones.",
+    "Terrible",
+    "I love this game",
+    "Pretty dull and repetitive.",
+    "nothing but ads smh",
+    "Nice 👍",
+    "One run, one ad.",
+    "So bad",
+    "I am not impressed by this",
+    "This is the worst game I have ever played and only exists to scam kids out of money through ads or their parents credit cards",
+    "App keeps closing...bye",
+    "too many ads",
+    "hella ads but still fun tho",
+    "Constantly crashes. Not playable.",
+    "Don't download... It's keep crashing",
+    "Crashed to much and had an ad every 10 seconds.",
+    "Its stupid",
+    "I said I didn't want an ad gave me one right after anyways",
+    "I like this game",
+    "Kul",
+    "It's more like there is a game in the ads than ads in the game",
+    "very cool",
+    "drag race.... 👍👍👍",
+    "ကိုသဲ",
+    "this game wasting our time",
+    "cool",
+    "Awful, an ad after every race. Couldn't double, couldn't get another car that I bet. Just frustrating overall.",
+    "Mythpat",
+    "I don't want it again!",
+    "Too many ads",
+    "MORE ADDS"
+];
+
+
+const v1Approach = (text) => {
+
+        // init
+        const spellCorrector = new SpellCorrector();
+
+        spellCorrector.loadDictionary('./dictionary.txt');
+
+        // expand contractions - e.g. I'm -> I am
+        const lexedReview = aposToLexForm(text);
+        // console.log(lexedReview);
+        // force to lower case
+        const casedReview = lexedReview.toLowerCase();
+        // console.log(casedReview);
+        // remove non letters
+        // const alphaOnlyReview = casedReview.replace(/[^a-zA-Z\s]+/g, '');
+        // const alphaOnlyReview = casedReview;
+        // console.log(alphaOnlyReview);
+        // string sentence to array of words
+        const { WordTokenizer } = natural;
+        const tokenizer = new WordTokenizer();
+        const tokenizedReview = tokenizer.tokenize(casedReview);
+        // console.log(tokenizedReview);
+        // spell check each word
+        tokenizedReview.forEach((word, index) => {
+            tokenizedReview[index] = spellCorrector.correct(word);
+        })
+        
+        // console.log(tokenizedReview);
+        // console.log(filteredReview);
+        // console.log(analysis);
+        
+        let result = isMatchedWord(tokenizedReview, casedReview);
+        if (result.matches.length > 0) {
+            // filter out stop words - e.g. but, a, what
+            const filteredReview = SW.removeStopwords(tokenizedReview);
+            // sentiment analysis
+            var sentiment = new Sentiment();
+            var options = {
+                extras: {
+                  'like': 0,
+                }
+              };
+            var analysis = sentiment.analyze(casedReview, options);
+            let modifiedResult = analysis.score + result.matches.value;
+            console.log(text);
+            console.log(modifiedResult);    // Sentiment
+            console.log('MATCH');
+            console.log(matches);
+            console.log('________');
+        }
+    
+}
+
+const analyse = (text) => {
     const lexedReview = aposToLexForm(text);
-    // console.log(lexedReview);
-    // force to lower case
     const casedReview = lexedReview.toLowerCase();
-    // console.log(casedReview);
-    // remove non letters
-    // const alphaOnlyReview = casedReview.replace(/[^a-zA-Z\s]+/g, '');
-    // const alphaOnlyReview = casedReview;
-    // console.log(alphaOnlyReview);
-    // string sentence to array of words
+
+
     const { WordTokenizer } = natural;
     const tokenizer = new WordTokenizer();
     const tokenizedReview = tokenizer.tokenize(casedReview);
-    // console.log(tokenizedReview);
-    // spell check each word
-    tokenizedReview.forEach((word, index) => {
-        tokenizedReview[index] = spellCorrector.correct(word);
-    })
-    // filter out stop words - e.g. but, a, what
-    const filteredReview = SW.removeStopwords(tokenizedReview);
-    // sentiment analysis
-    const { SentimentAnalyzer, PorterStemmer } = natural;
-    const analyzer = new SentimentAnalyzer('English', PorterStemmer, 'afinn');
-    const analysis = analyzer.getSentiment(filteredReview);
-    
-    console.log(text);
-    console.log(tokenizedReview);
-    console.log(filteredReview);
-    console.log(analysis);
 
-    let matches = isMatchedWord(tokenizedReview, casedReview);
-    if (matches.length > 0) {
-        console.log('MATCH');
-        console.log(matches);
+    return isMatchedWord(tokenizedReview);
+
+}
+
+const main = () => {
+
+    const out = reviews2.map((text) => {
+
+        const keywordCount = analyse(text);
+
+        return { text, keywordCount };
+    
+    });
+
+    console.log(out);
+
+    let keywordCount = {
+        grindy: 0,
+        adverts: 0,
+        lootboxes: 0,
+        timers: 0,
+        payToWin: 0,
+        freeToPlay: 0,
     }
 
-    console.log('________')
+    out.forEach((review) => {
+        for (const [key, value] of Object.entries(keywordCount)) {
+            keywordCount[key] += review.keywordCount[key];
+        }
+    })
+    console.log(keywordCount);   
 
-});
+};
+
+main();
+
 
